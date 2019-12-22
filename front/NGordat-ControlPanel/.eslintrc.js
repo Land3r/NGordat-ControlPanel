02a4375cd3b649e4fc4fd45fbee42ae703950bff
Nicolas Gordat <nicolas.gordat@some-email-provider.com>
2019-12-22 01:43:42 +0100

    New lint rules

--- front/NGordat-ControlPanel/.eslintrc.js
+++ front/NGordat-ControlPanel/.eslintrc.js
@@ -13,8 +13,9 @@ module.exports = {
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential',
-    '@vue/standard'
+    'plugin:vue/recommended',
+    '@vue/standard',
+    'plugin:i18n-json/recommended',
   ],
 
   // required to lint *.vue files
